cd7454c4e9252d1676b1f9eb65946b52a43d0a13
Jonathan Cowling <09cowlingj@gmail.com>
2019-03-29 22:14:43 +0000

    Add explicit provider versions

--- deploy/main.tf
+++ deploy/main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region                  = "eu-west-1"
   shared_credentials_file = "${path.root}/secrets/aws/credentials"
   profile                 = "default"
+  version = "2.3"
 }
+
+provider archive {
+  version = "1.2"
+}
+
+provider template {
+  version = "2.1"
+}
+
 module "api-gateway" {
   source = "./api-gateway"
   build_dir = "${var.build_dir}"
