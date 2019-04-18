d7f5891386d3728cad65ba95c5921ca4740d5f5a
Jonathan Cowling <09cowlingj@gmail.com>
2019-04-18 23:47:56 +0100

    Add show base url in deploy output

--- deploy/api-gateway/outputs.tf
+++ deploy/api-gateway/outputs.tf
@@ -2,6 +2,6 @@ output "api_key" {
   value = "${aws_api_gateway_api_key.key.value}"
 }
 
-output "api" {
-  value = "${data.template_file.api.rendered}"
+output "base_url" {
+  value = "${aws_api_gateway_deployment.inventory_tracker_deployment.invoke_url}"
 }
--- deploy/output.tf
+++ deploy/output.tf
@@ -1,3 +1,8 @@
 output "api_key" {
   value = "${module.api-gateway.api_key}"
 }
+
+output "base_url" {
+  value = "${module.api-gateway.base_url}"
+}
+
