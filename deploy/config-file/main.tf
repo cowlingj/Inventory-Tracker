f91c1420d78a09a7396a953e27db942c05eaa203
Jonathan Cowling <09cowlingj@gmail.com>
2019-04-21 18:25:41 +0100

    Remove postman config file (will rewrite index)

--- deploy/config-file/main.tf
+++ deploy/config-file/main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "postman" {
   sensitive_content = "${data.template_file.postman_env.rendered}"
-  filename = "${var.postman_dir}/environment_${terraform.workspace}.json"
+  filename = "${var.postman_dir}/secrets/environment_${terraform.workspace}.json"
 }
