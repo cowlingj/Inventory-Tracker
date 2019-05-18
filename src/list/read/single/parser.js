964fb25d8a5aaf21ea6bf56f3e47344fbc9829e1
Jonathan Cowling <09cowlingj@gmail.com>
2019-05-19 00:18:03 +0100

    Prettier (1 files)
    src/list/read/single/parser.js

--- src/list/read/single/parser.js
+++ src/list/read/single/parser.js
@@ -1,11 +1,7 @@
 import ParseError from "../../../util/errors/parse-error"
 
 export default function(event) {
-    if (
-        !event ||
-        !event.pathParameters ||
-        !event.pathParameters.id
-    ) {
+    if (!event || !event.pathParameters || !event.pathParameters.id) {
         throw new ParseError('cannot find path parameter "id"')
     }
 
